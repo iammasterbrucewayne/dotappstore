701aa6d80a64a3f91b57b6c869dba93c3e5b2d06
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2023-07-08 13:10:46 +0530

    add nova wallet

--- src/pages/api/get-projects.js
+++ src/pages/api/get-projects.js
@@ -35,6 +35,13 @@ const data = [
 		url: "https://www.subwallet.app/",
 		tags: ["Wallet"],
 	},
+	{
+		logo: "https://pbs.twimg.com/profile_images/1598685297956585474/RTf32Evr_400x400.jpg",
+		appname: "Nova Wallet",
+		appdescription: "Next-gen iOS/Android app for Polkadot & Kusamanetwork eco",
+		url: "https://novawallet.io/",
+		tags: ["Wallet"],
+	},
 ];
 
 export default async function handler(req, res) {
