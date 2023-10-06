e93db615ccfe54745e64907528a64d19b87e1c49
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2023-10-06 18:11:48 +0530

    fix main grid for deployment

--- src/pages/project/[id]/project-info/main-grid.js
+++ src/pages/project/[id]/project-info/main-grid.js
@@ -1,10 +1,17 @@
-import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+import {
+	Grid,
+	GridItem,
+	HStack,
+	Skeleton,
+	useBreakpointValue,
+} from "@chakra-ui/react";
 import ProjectLogo from "@/components/common/project-logo";
 import CTA from "./cta";
 import MainContent from "./main-content";
 import TagList from "./tag-list";
 import ImageCarousel from "./image-carousel";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { times } from "lodash";
 
 const MainGrid = ({
 	id,
@@ -15,6 +22,7 @@ const MainGrid = ({
 	url,
 	tags,
 	featured,
+	isLoaded,
 }) => {
 	const [isHidden, setIsHidden] = useState(true);
 
@@ -34,19 +42,92 @@ const MainGrid = ({
 			gap={4}
 		>
 			<GridItem align="center">
-				<ProjectLogo logo={logo} />
+				<Skeleton
+					isLoaded={isLoaded}
+					rounded="2xl"
+					startColor="pink.100"
+					endColor="pink.300"
+				>
+					<ProjectLogo logo={logo} />
+				</Skeleton>
 			</GridItem>
 			<GridItem colSpan={[1, 3, 3, 7]} alignContent={["center", "start"]}>
-				<MainContent
-					appname={appname}
-					appdescription={appdescription}
-					textAlign={["center", "left"]}
-				/>
-				<TagList tags={tags} justifyContent={["center", "start"]} mb={8} />
-				<ImageCarousel imageUrls={imageUrls} hidden={isHidden} />
+				{isLoaded ? (
+					<Fragment>
+						<MainContent
+							appname={appname}
+							appdescription={appdescription}
+							textAlign={["center", "left"]}
+							isLoaded={isLoaded}
+						/>
+						<TagList
+							tags={tags}
+							justifyContent={["center", "start"]}
+							mb={8}
+							isLoaded={isLoaded}
+						/>
+
+						<ImageCarousel imageUrls={imageUrls} hidden={isHidden} />
+					</Fragment>
+				) : (
+					<Fragment>
+						<Skeleton
+							w={60}
+							h={16}
+							mb={2}
+							rounded="2xl"
+							startColor="pink.100"
+							endColor="pink.300"
+						/>
+						<Skeleton
+							w="75%"
+							h={5}
+							mb={8}
+							rounded="md"
+							startColor="pink.100"
+							endColor="pink.300"
+						/>
+						<HStack>
+							{times(2, (index) => (
+								<Skeleton
+									key={index}
+									w={12}
+									h={6}
+									mb={8}
+									rounded="md"
+									startColor="pink.100"
+									endColor="pink.300"
+								/>
+							))}
+						</HStack>
+						<Skeleton
+							w={600}
+							h={400}
+							mb={4}
+							hidden={isHidden}
+							rounded="2xl"
+							startColor="pink.100"
+							endColor="pink.300"
+						/>
+					</Fragment>
+				)}
 			</GridItem>
 			<GridItem align="center" colSpan={[1, 1, 1, 2]}>
-				<CTA url={url} />
+				{isLoaded ? (
+					<CTA url={url} />
+				) : (
+					times(2, (index) => (
+						<Skeleton
+							key={index}
+							w="full"
+							h={16}
+							mb={4}
+							rounded="2xl"
+							startColor="pink.100"
+							endColor="pink.300"
+						/>
+					))
+				)}
 			</GridItem>
 		</Grid>
 	);
