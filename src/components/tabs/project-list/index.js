aa51f978752fd88fb21c066944e4a1694921a689
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2023-12-03 14:44:11 +0530

    add project count

--- src/components/tabs/project-list/index.js
+++ src/components/tabs/project-list/index.js
@@ -1,27 +1,30 @@
-import { Stack, StackDivider, TabPanel } from "@chakra-ui/react";
+import { Stack, StackDivider, TabPanel, Text } from "@chakra-ui/react";
 import ProjectItem from "./project-item";
 import _ from "lodash";
 
 export default function ProjectList({ category, projects }) {
-	const displayAll = category === "All";
-	const filteredProjects = displayAll
-		? projects
-		: _.filter(projects, (project) => _.includes(project.tags, category));
-	return (
-		<TabPanel>
-			<Stack
-				border="1px"
-				bg="blackAlpha.900"
-				color={"white"}
-				rounded={32}
-				overflow="hidden"
-				spacing={0}
-				divider={<StackDivider borderColor="gray.700" />}
-			>
-				{_?.map(filteredProjects, (_content, index) => (
-					<ProjectItem {..._content} key={index} />
-				))}
-			</Stack>
-		</TabPanel>
-	);
+  const displayAll = category === "All";
+  const filteredProjects = displayAll
+    ? projects
+    : _.filter(projects, (project) => _.includes(project.tags, category));
+  return (
+    <TabPanel>
+      <Text fontSize="sm" color="gray.600" mb={6}>
+        {_.size(filteredProjects)} projects filtered by "{category}"
+      </Text>
+      <Stack
+        border="1px"
+        bg="blackAlpha.900"
+        color={"white"}
+        rounded={32}
+        overflow="hidden"
+        spacing={0}
+        divider={<StackDivider borderColor="gray.700" />}
+      >
+        {_?.map(filteredProjects, (_content, index) => (
+          <ProjectItem {..._content} key={index} />
+        ))}
+      </Stack>
+    </TabPanel>
+  );
 }
